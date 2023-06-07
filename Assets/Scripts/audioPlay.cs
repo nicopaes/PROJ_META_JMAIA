4ee32346044504d5f7bd6472eab096de83d5eabd
Nicolas Paes Leme <nicolaspleme@gmail.com>
2023-06-07 18:46:14 -0300

    Commit with unity project

---
+++ Assets/Scripts/audioPlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioPlay : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "CollideObject")
+        {
+            audioSource.Play();
+        }
+    }
+
+}
