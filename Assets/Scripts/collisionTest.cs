3cec30fafd377624aaec2c49b6b57911eb38122d
Nicolas Paes Leme <nicolaspleme@gmail.com>
2023-07-02 13:53:57 -0300

    Player collision in CyanEmu or VRChat sym

--- Assets/Scripts/collisionTest.cs
+++ Assets/Scripts/collisionTest.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRC.SDK3.ClientSim;
 
 public class collisionTest : MonoBehaviour
 {   
     private void OnTriggerEnter(Collider other) 
     {
-        if(other.tag == "Player")
-        {
+        if(other.CompareTag("Player") || other.GetComponent<ClientSimBehaviour>() != null)
+        {   
             Debug.Log($"name: {other.transform.name}");
-        }        
+        }      
     }
 }
